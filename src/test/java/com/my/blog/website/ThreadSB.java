80b6dd91406409029fd7a6b8fcdc920252b352c1
dangjinhu <dangjinh@163.com>
2019-01-05 16:38:13 +0800

    add dependency

---
+++ src/test/java/com/my/blog/website/ThreadSB.java
@@ -0,0 +1,15 @@
+package com.my.blog.website;
+
+/**
+ * ************************************
+ *
+ * @ClassName: ThreadSB
+ * @Auther: dangjinhu
+ * @Date: 2019/1/5 16:35
+ * @Description:
+ * @Copyright: All rights reserver.
+ * ************************************
+ */
+
+public class ThreadSB {
+}
